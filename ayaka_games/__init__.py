171b81874bca11067156b6c00c5834a0a09c0d62
Su <wxlxy316@163.com>
2022-10-17 13:17:03 +0800

    0.1.2

--- ayaka_games/__init__.py
+++ ayaka_games/__init__.py
@@ -1,17 +1,4 @@
 # 导入全部插件
-from importlib import import_module
 from pathlib import Path
-from ayaka import logger
-
-paths = Path(__file__).parent.iterdir()
-
-for p in paths:
-    name = p.stem
-    if name.startswith("_") or name.startswith("."):
-        continue
-    module_name = f"ayaka_games.{name}"
-    try:
-        import_module(module_name)
-        logger.success(f"{name} 导入成功")
-    except:
-        logger.exception(f"{name} 导入失败")
+from ayaka.ayaka import load_plugins
+load_plugins(Path(__file__).parent, "ayaka_games")
