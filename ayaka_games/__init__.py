8240d190947e6a0ecb7ac22cda11e295f559f507
Su <wxlxy316@163.com>
2023-01-05 12:05:41 +0800

    恢复意外注释的代码

--- ayaka_games/__init__.py
+++ ayaka_games/__init__.py
@@ -1,17 +1,17 @@
-# # # 提前加载
-# import pypinyin
-# import re
+# # 提前加载
+import pypinyin
+import re
 
-# # 导入全部插件
-# from .plugins.bag import *
-# from .plugins.checkin import *
-# from .plugins.cy_query import *
-# from .plugins.get_30 import *
-# from .plugins.incan import *
-# from .plugins.mana import *
-# from .plugins.plus_one import *
-# from .plugins.who_is_suspect import *
-# from .plugins.genshin_random_event import *
-# from .plugins.dragon import *
-# from .plugins.calc_24 import *
-# from .plugins.word_tax import *
+# 导入全部插件
+from .plugins.bag import *
+from .plugins.checkin import *
+from .plugins.cy_query import *
+from .plugins.get_30 import *
+from .plugins.incan import *
+from .plugins.mana import *
+from .plugins.plus_one import *
+from .plugins.who_is_suspect import *
+from .plugins.genshin_random_event import *
+from .plugins.dragon import *
+from .plugins.calc_24 import *
+from .plugins.word_tax import *
