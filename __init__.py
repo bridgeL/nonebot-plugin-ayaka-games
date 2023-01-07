1aa474d22514285eb275a8669f4a4ceb5adc2240
Su <wxlxy316@163.com>
2023-01-07 14:24:31 +0800

    变更：修改子插件位置

--- __init__.py
+++ __init__.py
@@ -1,17 +1,6 @@
-# # 提前加载
+# 提前加载
 import pypinyin
 import re
 
 # 导入全部插件
-from .plugins.bag import *
-from .plugins.checkin import *
-from .plugins.cy_query import *
-from .plugins.get_30 import *
-from .plugins.incan import *
-from .plugins.mana import *
-from .plugins.plus_one import *
-from .plugins.who_is_suspect import *
-from .plugins.genshin_random_event import *
-from .plugins.dragon import *
-from .plugins.calc_24 import *
-from .plugins.word_tax import *
+from . import bag, checkin, cy_query, get_30, incan, mana, plus_one, who_is_suspect, genshin_random_event, dragon, calc_24, word_tax
